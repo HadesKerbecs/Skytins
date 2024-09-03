9d3983124793efcadb21ebd5fbfe1ece16490d0f
Eduardo Tristão <86086349+HadesKerbecs@users.noreply.github.com>
2024-09-03 13:54:25 -0300

    Update urls.py

--- skyusers/urls.py
+++ skyusers/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from .views import index, lista_clientes, adicionar_cliente, importar_dados
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', index, name='index'),
+    path('clientes/', lista_clientes, name='lista_clientes'),
+    path('adicionar/', adicionar_cliente, name='adicionar_cliente'),
+    path('importar/', importar_dados, name='importar_dados'),
 ]
