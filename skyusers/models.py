55980fade547567c124b5b9255fff05d8251a2a9
Eduardo Tristão <86086349+HadesKerbecs@users.noreply.github.com>
2024-09-03 13:54:14 -0300

    Update models.py

--- skyusers/models.py
+++ skyusers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    codigo = models.CharField(max_length=20)
+    nome = models.CharField(max_length=100)
+    ip = models.CharField(max_length=15)
+    cnpj = models.CharField(max_length=18)
+    qt_usuarios = models.IntegerField()
+    grupo = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nome
